a85ac1b161772e0477f216e8e674fc60fca52810
artistrea <apiravesi@gmail.com>
2022-11-15 16:59:04 -0300

    attaching image in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(email: "ava@ava.com", name: "ava", password: "123456")
+
+user.photo.attach(
+    io:  File.open(File.join(Rails.root,'app/assets/images/image.jpeg')),
+    filename: 'image.jpeg'
+)
